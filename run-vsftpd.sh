28798df3b26ae30d08e50efc9be70d0808e727b1
暮晨 <leisurelicht@gmail.com>
2021-01-15 10:00:13 +0800

    update

--- run-vsftpd.sh
+++ run-vsftpd.sh
@@ -22,6 +22,9 @@ fi
 mkdir -p "/home/vsftpd/${FTP_USER}"
 chown -R ftp:ftp /home/vsftpd/
 
+mkdir -p /var/log/vsftpd
+touch /var/log/vsftpd/vsftpd.log
+
 echo -e "${FTP_USER}\n${FTP_PASS}" > /etc/vsftpd/virtual_users.txt
 /usr/bin/db_load -T -t hash -f /etc/vsftpd/virtual_users.txt /etc/vsftpd/virtual_users.db
 
